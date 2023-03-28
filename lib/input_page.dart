bdd5b5876c38bbb9a30fcc3889e644c1f124c36d
may182 <220me2402@nitrkl.ac.in>
2023-03-28 11:38:51 +0530

    refactored flutter widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,8 @@
 
 
 
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -19,8 +21,45 @@ class _inputPageState extends State<inputPage> {
         title: Text('BMI Calculator'),
         backgroundColor: Color(0XFF141A3B),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+            children: <Widget>[
+              Expanded(child: Row(
+                children: <Widget>[
+                  Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+                  Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+                ],
+              )),
+              Expanded(child:ReusableCard(colour: Color(0xFF1D1E33))),
+              Expanded(child:Row(
+              children: <Widget>[
+              Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+              Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+            ],
+      )
+              ),
+            ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  // const ReusableCard({
+  //   super.key,
+  // });
+  // Will define our custom constructor
+
+  const ReusableCard({required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
